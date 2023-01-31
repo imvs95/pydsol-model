7861e97e69f411b0b2f93d6b3920744d13a431ce
imvanschilt <i.m.vanschilt@tudelft.nl>
2023-01-31 14:02:28 +0100

    add setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+REQUIREMENTS = (HERE / "requirements.txt").read_text().splitlines()
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="pydsol-model",
+    version="1.0",
+    description=open("README.md").read(),
+    author="Isabelle M. van Schilt",
+    url="https://github.com/imvs95/pydsol-model",
+    packages=find_packages(),
+    install_requires=REQUIREMENTS
+)
